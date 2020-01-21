79a6558df2023e1ef1005d1b92079eb1a947880b
nick <nicholas.johnston@sheridancollege.ca>
2020-01-21 09:43:44 -0500

    Finished base example

--- web/app/app.py
+++ web/app/app.py
@@ -1,14 +1,26 @@
 import os
 import socket
-
 from flask import Flask
+from flask import render_template, flash
 from redis import Redis
+from app.forms import SimpleForm
 
-
-app = Flask(__name__)
+app = Flask(__name__,
+        static_url_path='',
+        static_folder='static',
+        template_folder='templates')
+app.config['SECRET_KEY'] = 'this-is-a-secret-key-you-generate-for-your-app'
 redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
-
 
 @app.route('/')
 def hello():
     return "Hello World from Python in a Flask app in a Docker container"
+
+@app.route('/simpleform', methods=['GET','POST'])
+def simple_form():
+    sform = SimpleForm()
+    if sform.validate_on_submit():
+        redis.incr("hits")
+        flash(f"Hi {sform.username.data}")
+    return render_template("simpleform.html", title="Simple Form", form=sform, hits=int(redis.get("hits")))
+
--- web/runserver.sh
+++ web/runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn --log-level info --log-file=/gunicorn.log --workers 4 --name app -b 0.0.0.0:8000 --reload app.app:app
+gunicorn --log-level info --log-file=- --workers 1 --name app -b 0.0.0.0:8000 --reload app.app:app 
